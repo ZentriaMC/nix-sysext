2f2bd1c20cef0391e50ddab26d46b8778d1714f0
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-08-14 03:06:19 +0300

    Progress

--- flake.nix
+++ flake.nix
@@ -1,5 +1,22 @@
 {
   description = "nix-sysext";
 
-  outputs = { self, nixpkgs }: { };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    in
+    {
+      lib = {
+        mkSysExt = import ./make-sysext.nix;
+      } // nixpkgs.lib.genAttrs supportedSystems (system: {
+        mkSysExt = args@{ name, packages }: nixpkgs.legacyPackages.${system}.callPackage (self.lib.mkSysExt args) { };
+      });
+    };
 }
